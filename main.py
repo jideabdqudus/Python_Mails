c7d3bee52d0a7de157f24431b6200ed73e2e5cfb
jideabdqudus <jideabdqudus@gmail.com>
2021-04-13 14:52:40 +0100

    Email with HTML

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import smtplib
 
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+html = Template(Path("index.html").read_text())
 
 email = EmailMessage()
-email['from'] = "Qudusini De' Boss"
+email['from'] = "Qudusini"
 email['to'] = "hello@abdulqudus.com"
-email['subject'] = 'Random note of Love'
+email['subject'] = "Welcome to our company"
 
-email.set_content("Love yourz, - There's no life that's better than yourz")
+email.set_content(html.substitute({'name': 'Abdul'}), 'html')
+# email.set_content("Love yourz, - There's no life that's better than yourz")
+
+print("Begin")
 
 with smtplib.SMTP(host="smtp.gmail.com", port=587) as smtp:
+    print("Start")
     smtp.ehlo()
     smtp.starttls()
     smtp.login("justleftjupiter@gmail.com", "abdqudus100")
     smtp.send_message(email)
-    #smtp.sendmail(email)
     print("All Good")
